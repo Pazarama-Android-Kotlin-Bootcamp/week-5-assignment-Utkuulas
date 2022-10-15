4a55693ee0c82d4b6214244e641f0cea2b4ff391
Mert Toptaş <merttoptas34@gmail.com>
2022-10-15 13:16:12 +0300

    binding adapters added

--- app/src/main/java/com/merttoptas/retrofittutorial/ui/posts/PostsFragment.kt
+++ app/src/main/java/com/merttoptas/retrofittutorial/ui/posts/PostsFragment.kt
@@ -6,7 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.BindingAdapter
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.RecyclerView
 import com.merttoptas.retrofittutorial.data.api.ApiClient
 import com.merttoptas.retrofittutorial.data.model.Post
 import com.merttoptas.retrofittutorial.databinding.FragmentPostsBinding
@@ -29,10 +31,26 @@ class PostsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel.postLiveData.observe(viewLifecycleOwner) {
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        /*
+        Way 2
+         viewModel.postLiveData.observe(viewLifecycleOwner) {
             binding.rvPostsList.adapter = PostsAdapter().apply {
                 submitList(it)
             }
+        }
+
+         */
+    }
+}
+
+@BindingAdapter("app:postList")
+fun setPostList(recyclerView: RecyclerView, postList: List<Post>?) {
+    postList?.let {
+        recyclerView.adapter = PostsAdapter().apply {
+            submitList(it)
         }
     }
 }
